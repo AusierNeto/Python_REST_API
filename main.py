fb1e7b813063e16179c74b7f127ceb440a94bfa5
AusierNeto <ausierneto@gmail.com>
2025-10-28 22:26:44 -0400

    Feat: finish clients get and create routes

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from routers.clients import router as clients_router
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
 app.include_router(clients_router)
--- routers/clients.py
+++ routers/clients.py
@@ -7,8 +7,15 @@ from data.session import get_db
 router = APIRouter(prefix="/clients", tags=["clients"])
 
 @router.get("/")
-async def get_clients():
-    return {"message": "List of clients"}
+async def get_clients(db=Depends(get_db)) -> list[ClientSchema]:
+    clients = db.query(Client).all()
+    return [ClientSchema(
+        id=client.id,
+        name=client.name,
+        email=client.email,
+        created_at=client.created_at,
+        updated_at=client.updated_at
+    ) for client in clients]
 
 @router.post("/")
 async def create_client(data:ClientSchema, db=Depends(get_db)):
@@ -22,3 +29,4 @@ async def create_client(data:ClientSchema, db=Depends(get_db)):
     db.commit()
     db.refresh(new_client)
     return {"message": "Client created", "client": data}
+
